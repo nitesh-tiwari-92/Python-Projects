4b306c39e39e931204270c0dff9618ba933829ec
Nitesh Tiwari <niteshtiwari92@gmail.com>
2026-03-16 22:19:27 +0530

    Update Arithmetic_Operations.py
    Signed-off-by: Nitesh Tiwari <niteshtiwari92@gmail.com>

--- Calculator with Modules & Looping Functions/Arithmetic_Operations.py
+++ Calculator with Modules & Looping Functions/Arithmetic_Operations.py
@@ -56,11 +56,6 @@ def div():
         print("The second provided number must be greater than Zero (0).\n")
 
         logging.error("Attempted to divide by zero.", exc_info=True)
-        # with open('Error_Logger.log', 'r') as fr: 
-        #     data = fr.read()
-
-        # with open(r'C:\Users\Nitesh_Tiwari\Downloads\Error_Log.txt', 'w') as fw:
-        #     fw.write(data)
 
     except Exception as e:
         print(f"\nSomething went wrong. Please try again. Error: {e}\n")
@@ -100,87 +95,3 @@ def floor_div():
         print(f"\nSomething went wrong. Please try again. Error: {e}\n")
         logging.error("An error occured while Floor Division", exc_info=True)
 
-
-# def mul_add(statement):
-
-#     # user_num = input(statement).split(' ')
-#     # if len(user_num) <= 1:
-
-#     #     try:
-#     #         if user_num[0].isnumeric():
-#     #             user_num[0] = int(user_num[0])
-#     #         else:
-#     #             user_num[0] = float(user_num[0])
-
-#     #         if user_num[0] >= 10:
-#     #             user_ch = continuation("Do you want the numbers to be separated individually and after perform the addition? (Y/N): ")
-
-#     #             if user_ch == 'Y':
-#     #                 user_num = list(str(user_num[0]))
-#     #                 user_num = [int(num) for num in user_num]
-
-#     #                 result = 0
-#     #                 for i in user_num:
-#     #                     result += i
-#     #             # print(f"The Addition of the Individual Digits of the Number is: {result}\n")
-
-#     #             else:
-#     #                 return mul_add(statement)
-
-#     #         else:
-#     #             return mul_add(statement)
-#     #         # else:
-                
-#     #     except Exception:
-#     #         print("Please Enter a Valid Numeric Number.\n")
-#     #         return mul_add(statement)
-
-#     # else:
-#     #     print("Please Enter Only One Number for this Operation.\n")
-#     #     return mul_add(statement)
-
-#     while True:
-
-#         user_num = input(statement).split(' ')
-
-#         try:
-#             num = int(user_num[0])
-
-#         except ValueError:
-#             print("Please Enter a Valid Integer Number.\n")
-#             continue
-
-#         if num < 10:
-#             print("Number must be 10 or greater.\n")
-#             continue
-
-#         user_ch = continuation("Do you want the digits to be separated and added? (Y/N): ")
-
-#         if user_ch == 'Y':
-
-#             digits = [int(d) for d in str(num)]
-#             result = sum(digits)
-
-#             print(f"The Addition of the Individual Digits is: {result}\n")
-#             return result
-
-#         else:
-#             print("Operation cancelled.\n")
-#             return None
-
-#         if len(user_num) > 1:
-#             print("Please Enter Only One Number for this Operation.\n")
-#             continue
-
-
-
-# def factorial():
-#     fact_1 = 1
-#     fact_2 = 1
-#     for i in range(1, num_1 + 1):
-#         fact_1 *= i
-#     for j in range(1, num_2 + 1):
-#         fact_2 *= j
-#     print(f"Factorial of {num_1} is:", fact_1)
-#     print(f"Factorial of {num_2} is:", fact_2)
-#     return print("\nThe Factorial of the Two Numbers is:", fact_1, "and", fact_2,"\n")
