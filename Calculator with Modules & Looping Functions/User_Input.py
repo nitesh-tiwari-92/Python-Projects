856cf04112a51c5942c1e8013b6ad0a8869d6b15
Nitesh Tiwari <niteshtiwari92@gmail.com>
2026-04-01 19:11:07 +0530

    Added multiple input function
    Signed-off-by: Nitesh Tiwari <niteshtiwari92@gmail.com>

--- Calculator with Modules & Looping Functions/User_Input.py
+++ Calculator with Modules & Looping Functions/User_Input.py
@@ -1,6 +1,4 @@
 
-
-
 def single_input(statement):
 
     try:     
@@ -32,8 +30,76 @@ def double_input():
 
 def continuation(statement):
 
-    user_ch = input(statement)
-    if user_ch == 'Y' or user_ch == 'y':
-        return user_ch.upper()
+    user_ch = input(statement).lower()
+    if user_ch in ['y', 'ye', 'yes']:
+        return user_ch
     else:
         exit
+
+
+def separate():
+    
+    print('\nCalclulation with single number is not possible! Please provide atleast 2 number to perform the calculation.\n')
+    ch = input("Do you want the numbers to be separated individually and after perform the addition? (Y/N): ").lower()
+
+    if ch in ['y', 'yes', 'ye']:
+        return ch
+
+    else:
+        print('Thank you for choosing the Nitesh\'s Calculator!!')
+        print('Please comeback later.')
+        exit
+            
+
+def mul_input(*numbers):
+     
+    try:
+        user_input = (input('\nEnter your Numbers (separated by single (only one) blank space): ').strip()).lstrip()
+        user_input = user_input.split(' ')
+
+        for n in range(len(user_input)):
+
+            if user_input[n] == '' or user_input[n].isspace():
+                # user_input[n] = user_input[n].replace('', '0')
+                # user_input[n] = int(user_input[n])
+                print('\nPlease provide Numbers sepatated by only one/ single space (duplicate spaces in between are not allowed).\n')
+                return mul_input(*numbers)
+
+            elif user_input[n].isnumeric():
+                user_input[n] = int(user_input[n])
+
+            else:
+                user_input[n] = float(user_input[n])
+
+        if not len(user_input) >= 2:
+
+            print('\nCalclulation with single number is not possible! Please provide atleast 2 number to perform the calculation.\n')
+            ch = input("Do you want the numbers to be separated individually and after perform the calculation? (Y/N): ").lower()
+
+            if ch in ['y', 'yes', 'ye']:
+                lst = []
+                user_input = str(user_input[0])
+                # print(user_input, type(user_input), len(user_input))
+                
+                for n in range(len(user_input)):
+                    lst.append(user_input[n])
+                    lst[n] = int(lst[n])
+                
+                user_input = lst
+                return user_input
+
+            else:
+                print('\nThank you for choosing the Nitesh\'s Calculator!!')
+                print('Please comeback later.\n')
+                exit()
+
+        else:
+            return user_input
+
+    except ValueError:
+        print("Please Enter a Valid Numeric Number.\n")
+        return mul_input(*numbers)
+
+    except Exception as e:
+        print(f"Error:", e, '\nPlease Try Again: \n')
+        return mul_input(*numbers)
